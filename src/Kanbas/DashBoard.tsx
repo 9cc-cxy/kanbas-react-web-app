31ad17edc3b5ee7ff273d918c127f9acfb2f7af4
Josie Cheng <josie_chengxinyu@outlook.com>
2024-09-19 20:14:06 -0400

    added format

--- src/Kanbas/DashBoard.tsx
+++ src/Kanbas/DashBoard.tsx
@@ -6,13 +6,13 @@ export default function Dashboard() {
       <h2 id="wd-dashboard-published">Published Courses (8)</h2> <hr />
       <div id="wd-dashboard-courses">
         <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/1234/Home">
+          <Link
+            className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/1234/Home"
+          >
             <img src="images/react.png" width={200} />
             <div>
-              <h5>
-                CS1234
-              </h5>
+              <h5>CS1234</h5>
               <p className="wd-dashboard-course-title">
                 Full Stack software developer
               </p>
@@ -21,43 +21,39 @@ export default function Dashboard() {
           </Link>
         </div>
         <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/2345/Home">
+          <Link
+            className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/2345/Home"
+          >
             <img src="images/sql.png" width={200} />
             <div>
-              <h5>
-                CS2345
-              </h5>
-              <p className="wd-dashboard-course-title">
-                Database Management
-              </p>
+              <h5>CS2345</h5>
+              <p className="wd-dashboard-course-title">Database Management</p>
               <button> Go </button>
             </div>
           </Link>
         </div>
         <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/3456/Home">
+          <Link
+            className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/3456/Home"
+          >
             <img src="images/algo.png" width={200} />
             <div>
-              <h5>
-                CS3456
-              </h5>
-              <p className="wd-dashboard-course-title">
-                Algorithms
-              </p>
+              <h5>CS3456</h5>
+              <p className="wd-dashboard-course-title">Algorithms</p>
               <button> Go </button>
             </div>
           </Link>
         </div>
         <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/1000/Home">
+          <Link
+            className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/1000/Home"
+          >
             <img src="images/ai.png" width={200} />
             <div>
-              <h5>
-                CS1000
-              </h5>
+              <h5>CS1000</h5>
               <p className="wd-dashboard-course-title">
                 Artificial Intelligence
               </p>
@@ -66,13 +62,13 @@ export default function Dashboard() {
           </Link>
         </div>
         <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/5299/Home">
+          <Link
+            className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/5299/Home"
+          >
             <img src="images/ml.png" width={200} />
             <div>
-              <h5>
-                CS5299
-              </h5>
+              <h5>CS5299</h5>
               <p className="wd-dashboard-course-title">
                 Introduction to Machine Learning
               </p>
@@ -81,13 +77,13 @@ export default function Dashboard() {
           </Link>
         </div>
         <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/5004/Home">
+          <Link
+            className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/5004/Home"
+          >
             <img src="images/oop.jpg" width={200} />
             <div>
-              <h5>
-                CS5004
-              </h5>
+              <h5>CS5004</h5>
               <p className="wd-dashboard-course-title">
                 Object-Oriented Programming
               </p>
@@ -96,28 +92,26 @@ export default function Dashboard() {
           </Link>
         </div>
         <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/5690/Home">
+          <Link
+            className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/5690/Home"
+          >
             <img src="images/security.jpg" width={200} />
             <div>
-              <h5>
-                 CS5690
-              </h5>
-              <p className="wd-dashboard-course-title">
-                Cybersecurity
-              </p>
+              <h5>CS5690</h5>
+              <p className="wd-dashboard-course-title">Cybersecurity</p>
               <button> Go </button>
             </div>
           </Link>
         </div>
         <div className="wd-dashboard-course">
-          <Link className="wd-dashboard-course-link"
-                to="/Kanbas/Courses/5001/Home">
+          <Link
+            className="wd-dashboard-course-link"
+            to="/Kanbas/Courses/5001/Home"
+          >
             <img src="images/python.png" width={200} />
             <div>
-              <h5>
-                CS5001
-              </h5>
+              <h5>CS5001</h5>
               <p className="wd-dashboard-course-title">
                 Introduction to Programming
               </p>
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -24,6 +24,5 @@ export default function Kanbas() {
         </tr>
       </table>
     </div>
-);}
-
-
+  );
+}
