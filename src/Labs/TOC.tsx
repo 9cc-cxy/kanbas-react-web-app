1b2ccb4b876b464474ea6572ee643bc9a1f469ee
Josie Cheng <josie_chengxinyu@outlook.com>
2024-11-22 15:11:23 -0500

    finished a5

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -49,11 +49,6 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-k" href="https://github.com/9cc-cxy" className="nav-link">
-          My GitHub
-        </a>
-      </li>
-      <li className="nav-item">
         <a id="wd-k" href="https://kanbas-node-server-app-nmz2.onrender.com/" className="nav-link">
           kanbas-node-server-app
         </a>
