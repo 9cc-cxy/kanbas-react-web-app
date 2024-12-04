13ef42e1ecc7635e8c4af8d667de3b01fcecd398
Josie Cheng <josie_chengxinyu@outlook.com>
2024-12-03 20:24:10 -0500

    4.1.2

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -5,33 +5,15 @@ export default function AccountNavigation() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
   const { pathname } = useLocation();
+  const active = (path: string) => (pathname.includes(path) ? "active" : "text-danger");
 
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-      <Link
-        to={`/Kanbas/Account/Signin`}
-        className={`list-group-item border border-0 
-      ${pathname.includes("Signin") ? "active" : "text-danger"}`}
-      >
-        {" "}
-        Signin{" "}
-      </Link>
-      <Link
-        to={`/Kanbas/Account/Signup`}
-        className={`list-group-item border border-0 
-      ${pathname.includes("Signup") ? "active" : "text-danger"}`}
-      >
-        {" "}
-        Signup{" "}
-      </Link>
-      <Link
-        to={`/Kanbas/Account/Profile`}
-        className={`list-group-item border border-0 
-      ${pathname.includes("Profile") ? "active" : "text-danger"}`}
-      >
-        {" "}
-        Profile{" "}
-      </Link>
+      {links.map((link) => (
+       <Link key={link} to={`/Kanbas/Account/${link}`} className={`list-group-item border border-0 ${active(link)}`}> {link} </Link>
+     ))}
+     {currentUser && currentUser.role === "ADMIN" && (
+       <Link to={`/Kanbas/Account/Users`} className={`list-group-item border border-0 ${active("Users")}`}> Users </Link> )}
     </div>
   );
 }
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -6,12 +6,23 @@ import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
 import { FaAlignJustify } from "react-icons/fa";
 import PeopleTable from "./People/Table";
+import * as accountClient from "../Account/client";
+import { useEffect, useState } from "react";
 
 export default function Courses({ courses }: { courses: any[]; }) {
-  const { cid } = useParams();
+  const { cid, uid } = useParams();
   const course = courses.find((course) => course._id === cid);
   const { pathname } = useLocation();
-  
+  const [users, setUsers] = useState<any[]>([]);
+
+  const fetchUsers = async () => {
+    const users = await accountClient.findAllUsers();
+    setUsers(users);
+  }; 
+  useEffect(() => {
+    fetchUsers();
+  }, [uid]);
+
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
@@ -30,7 +41,7 @@ export default function Courses({ courses }: { courses: any[]; }) {
             <Route path="Modules" element={<Modules />} />
             <Route path="Assignments" element={<Assignments />} />
             <Route path="Assignments/:aid" element={<AssignmentEditor />} />
-            <Route path="People" element={<PeopleTable />} />
+            <Route path="People" element={<PeopleTable users={users}/>} />
           </Routes>
         </div>
       </div>
